a301755067c3a26110b78354eae9f5b5cdb74127
Diego Cornejo <cornejodl13@icloud.com>
2023-11-28 17:21:54 -0600

    worked on initial setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Circle, Triangle, Square } = require("./lib/shape.js");
+
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What shape do you want your logo",
+            choices: ["Circle", "Triangle", "Rectangle"],
+            name: "shape",
+        },
+        {
+            type: "input",
+            message: "What background color would you like",
+            name: "color",
+        },
+        {
+            type: "input",
+            message: "Enter 3 letters to be on Logo",
+            name: "text",
+        },
+        {
+            type: "input",
+            message: "Enter color for text",
+            name: "textColor",
+        },
+    ])
+    .then((responses) => {
+        let shape;
+        if (responses.shape === "Circle"){
+            shape = new Circle(
+                responses.color,
+                responses.text,
+                responses.textColor
+                );
+        } else if (responses.shape === "Square"){
+            shape = new Square(
+                responses.color,
+                responses.text,
+                responses.textColor
+                );
+        } else if (responses.shape === "Triangle"){
+            shape = new Triangle(
+                responses.color,
+                responses.text,
+                responses.textColor
+                );
+        }
+    })
